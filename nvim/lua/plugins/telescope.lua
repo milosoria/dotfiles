2174ef1a665b7211e9ef08c3f0f343c251cbdea6
milosoria <camilo.soria@uc.cl>
2021-09-28 17:41:39 -0300

    feat(vimrc): telescope new mappings and use of builtins

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,10 +7,7 @@ local function init()
             file_ignore_patterns = {".git/","./node_modules/*", "node_modules", "^node_modules/*", "node_modules/*", "./yarn.*", "yarn.*"},
             selection_caret = "> ",
             prompt_prefix = "🔍 ",
-            entry_prefix = "  ",
             initial_mode = "insert",
-            selection_strategy = "reset",
-            sorting_strategy = "descending",
             layout_strategy = "horizontal",
             layout_config = {
                 width = 0.80,
@@ -23,16 +20,6 @@ local function init()
                     mirror = false,
                 },
             },
-            color_devicons = true,
-            winblend = 0,
-            border = {},
-            borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-            use_less = true,
-            --this is new (default nil)
-            set_env = { ['COLORTERM'] = 'truecolor' },
-            file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-            grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-            qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
             extensions = {
                 fzf = {
                     fuzzy = true,
@@ -53,7 +40,7 @@ local function init()
     -- neovim config directory
     vim.api.nvim_set_keymap('n', '<Leader>v', ':Telescope find_files cwd=~/.config/nvim<CR>', { noremap = true})
     -- global
-    vim.api.nvim_set_keymap('n', '<Leader>g', ':Telescope find_files theme=ivy cwd=~ <CR>', { noremap = true})
+    vim.api.nvim_set_keymap('n', '<Leader>g', ':Telescope find_files hidden=true theme=ivy cwd=~ <CR>', { noremap = true})
     -- buffers
     vim.api.nvim_set_keymap('n', '<Leader>b', ':Telescope buffers<CR>', { noremap = true})
 end
