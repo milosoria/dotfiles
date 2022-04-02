e2a0fa5cf81bbe966b2e6134bee625c451d15afe
milosoria <camilo.soria@uc.cl>
2022-04-02 13:43:48 -0300

    feat(vimrc): toggleterm new keymaps

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ local function init()
     vim.api.nvim_set_keymap('n', '<Leader>tn', ':ToggleTerminal<CR>', { noremap = true})
     vim.api.nvim_set_keymap('n', '<Leader>tv', ':vsplit | :ToggleTerminal<CR>', { noremap = true})
     vim.api.nvim_set_keymap('n', '<Leader>th', ':split | :ToggleTerminal<CR>', { noremap = true})
-    vim.g.open_in_insert_mode=0
+    vim.g.open_in_insert_mode=1
     vim.g.preserve_alternate_buffer=1
 end
 
