7ec1a1b3ff946c277766fff22f3d5bafc533ee81
milosoria <camilo.soria@uc.cl>
2022-11-02 10:10:55 -0300

    fix(vimrc): null-ls now formats async

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,15 +4,15 @@ local function init()
     local diagnostics = null_ls.builtins.diagnostics
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-        sources = {
-            formatting.stylua,
-            formatting.prettier,
-            diagnostics.eslint_d,
-        },
-        on_attach = function(current_client, bufnr)
-            if current_client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        null_ls.setup({
+            sources = {
+                formatting.stylua,
+                formatting.prettier,
+                diagnostics.eslint_d,
+            },
+            on_attach = function(current_client, bufnr)
+                if current_client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
                 vim.api.nvim_create_autocmd("BufWritePre", {
                     group = augroup,
                     buffer = bufnr,
@@ -22,6 +22,7 @@ local function init()
                                 return client.name == "null-ls"
                             end,
                             bufnr = bufnr,
+                            async = true,
                         })
                     end,
                 })
