5fc5d2e89b23930ae517c8caead496db6589cd78
milosoria <camilo.soria@uc.cl>
2025-09-24 16:15:10 -0300

    feat(nvim): update mason ensure installed configuration

--- nvim/lua/plugins/mason-workaround.lua
+++ nvim/lua/plugins/mason-workaround.lua
@@ -1,4 +1,46 @@
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    opts = {
+      ensure_installed = {
+        -- Go
+        "goimports",
+        "gofumpt",
+        "gopls",
+
+        -- Shell/Bash
+        "bash-language-server",
+        "shfmt",
+
+        -- Python
+        "black",
+        "blackd-client",
+        "isort",
+        "pyright",
+        "python-lsp-server",
+        "ruff",
+
+        -- JavaScript/TypeScript
+        "eslint-lsp",
+        "eslint_d",
+        "prettierd",
+        "typescript-language-server",
+
+        -- Web
+        "json-lsp",
+        "tailwindcss-language-server",
+
+        -- Lua
+        "lua-language-server",
+        "stylua",
+
+        -- Markdown
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+      },
+    },
+  },
   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
